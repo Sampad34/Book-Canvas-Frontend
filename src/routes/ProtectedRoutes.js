de54517ae45ad3890937cda6c99dbf7a6882c27c
sampad roy <sampadrb@gmail.com>
2025-09-26 16:37:48 +0530

    authservice edited fourth

--- src/routes/ProtectedRoutes.js
+++ src/routes/ProtectedRoutes.js
@@ -1,8 +1,7 @@
 import { Navigate } from "react-router-dom";
 
+export const ProtectedRoutes = ({ children }) => {
+  const token = sessionStorage.getItem("token"); // no JSON.parse
 
-export const ProtectedRoutes = ({children}) => {
-  const token = JSON.parse(sessionStorage.getItem("token"));
-
-  return token ? children : <Navigate to="/login" />
-}
+  return token ? children : <Navigate to="/login" />;
+};
